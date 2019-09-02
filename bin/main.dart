748d5edcfca174bfa76ad9c9977f1b529f867589
iabhishek3 <abhishek.111002@gmail.com>
2019-09-02 16:22:44 +0800

    dartlang

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_lang_core/dart_lang_core.dart' as dart_lang_core;
+
+main(List<String> arguments) {
+  print('Hello world: ${dart_lang_core.calculate()}!');
+}
---
+++ test/dart_lang_core_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_lang_core/dart_lang_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
